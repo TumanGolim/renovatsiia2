b58ec2e7fb6dbfac1d3e6d55fd65a4e89b9b54f2
TumanGolim <130469755+TumanGolim@users.noreply.github.com>
2025-03-16 16:45:36 +0200

    location

--- src/app/layouts/topbar/topbar.component.ts
+++ src/app/layouts/topbar/topbar.component.ts
@@ -11,9 +11,46 @@ import { CommonModule } from '@angular/common';
 export class TopbarComponent {
   isDarkMode = false;
 
+  cities = [
+    'Київська область',
+    'Вінницька область',
+    'Волинська область',
+    'Дніпропетровська область',
+    'Донецька область',
+    'Житомирська область',
+    'Закарпатська область',
+    'Запорізька область',
+    'Івано-Франківська область',
+    'Кіровоградська область',
+    'Луганська область',
+    'Львівська область',
+    'Миколаївська область',
+    'Одеська область',
+    'Полтавська область',
+    'Рівненська область',
+    'Сумська область',
+    'Тернопільська область',
+    'Харківська область',
+    'Херсонська область',
+    'Черкаська область',
+    'Чернігівська область',
+    'Чернівецька область',
+    'Автономна Республіка Крим',
+  ];
+  selectedCity = this.cities[0];
+  isCityDropdownOpen = false;
+
   toggleTheme(): void {
     this.isDarkMode = !this.isDarkMode;
-    console.log('Dark mode:', this.isDarkMode);
     document.body.classList.toggle('dark-theme', this.isDarkMode);
+  }
+
+  toggleCityDropdown(): void {
+    this.isCityDropdownOpen = !this.isCityDropdownOpen;
+  }
+
+  selectCity(city: string): void {
+    this.selectedCity = city;
+    this.isCityDropdownOpen = false;
   }
 }
